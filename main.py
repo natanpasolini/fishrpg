5c43ededd7b593ff87a81b6e749db7fb0e552055
Natan Pasolini <natanpasoliniob@gmail.com>
2025-10-15 17:31:05 -0300

    a01b
    add novos stats, ajustes nas chances, peixes renomeados, adicionado tamanhos de peixe, dicionario de peixes coletados pelo jogador com tamanho, menu refeito (com barra de xp), alteração de loop de menu para corrigir caso usuario digite nada

--- main.py
+++ main.py
@@ -1,41 +1,82 @@
-import pandas as pd
 import time
 import os
 import random
-
-MAX_OPTIONS = 2
+import math
 
 PLAYER_STATS = {
     "rod": 1,
-    "str": 0,
-    "luck": 0,
-    "speed": 0,
+    "str": 0.5,
+    "luck": 1,
+    "speed": 0.5,
+    "money": 5,
+    "level": 1,
+    "xp": 0,
+    "xplvlup": 2,
+    "sp": 0
 }
 
 BASE_CHANCES = {
-    "Secret": 0.1,
-    "Legendary": 1,
-    "Epic": 4,
-    "Rare": 10,
-    "Uncommon": 24.9,
-    "Common": 60,
+    "Secret": 0.01,
+    "Legendary": 0.1,
+    "Epic": 2,
+    "Rare": 7.9,
+    "Uncommon": 20,
+    "Common": 69.99
 }
 
 PEIXES = {
-    "Secret": ["Tubarao"],
-    "Legendary": ["Peixe Morto-vivo"],
-    "Epic": ["Peixe Borboleta"],
-    "Rare": ["Salmao"],
-    "Uncommon": ["Peixe Palhaco","Bota"],
-    "Common": ["Carpa","Atum"]
+    "Secret": ["Megalodonte (Filhote)","Enguia Elétrica Titânica"],
+    "Legendary": ["Pirarucu","Dourado (Rei do Rio)","Garoupa-verdadeira","Tubarão (Filhote)"],
+    "Epic": ["Peixe Borboleta","Corvina","Robalo","Tainha"],
+    "Rare": ["Pescadinha","Traíra","Parati"],
+    "Uncommon": ["Peixe Palhaco","Manjuba","Neon Cardinal","Peixe Agulhinha","Lambari"],
+    "Common": ["Alga","Larva","Camarão","Lambari"]
 }
+
+FISHES_SIZES = {
+    "Secret": [500,1000],
+    "Legendary": [100,130],
+    "Epic": [40,60],
+    "Rare": [12,40],
+    "Uncommon": [5,9],
+    "Common": [0,4]
+}
+
+PLAYER_FISHES = {}
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def startup():
+    limpar_tela()
+    print("====================")
+    print("\n  PY FISHING GAME")
+    print("  v. A01\n")
+    print("====================")
+    os.system("pause")
+    limpar_tela()
+
+def player_status_menu():
+    PLAYER_STATS["xplvlup"] = math.ceil((PLAYER_STATS["level"] + 1) * 3.75)
+    print(f"NÍVEL: {PLAYER_STATS['level']}   DINHEIRO: {PLAYER_STATS['money']}")
+    barraXP = ""
+    tamanho_total_barra = 16
+    if PLAYER_STATS["xp"] > 0:
+        dif = (PLAYER_STATS["xp"] * 100) / PLAYER_STATS["xplvlup"]
+        blocos_preenchidos = int(tamanho_total_barra * (dif / 100))
+        if blocos_preenchidos <= 0:
+            blocos_preenchidos = 1
+        blocos_vazios = tamanho_total_barra - blocos_preenchidos
+        barraXP = ("■" * blocos_preenchidos) + ("□" * blocos_vazios)
+    else:
+        barraXP = "□" * tamanho_total_barra
+    print(f"XP: {barraXP} {PLAYER_STATS['xp']}/{PLAYER_STATS['xplvlup']}")
 
 def calcular_raridades():
     rod_bonus = PLAYER_STATS["rod"] * 0.3
     str_bonus = PLAYER_STATS["str"] * 0.05
     luck_bonus = PLAYER_STATS["luck"] * 0.2
     BONUS_TOTAL = rod_bonus + str_bonus + luck_bonus
-    print(BONUS_TOTAL)
     CHANCES = BASE_CHANCES.copy()
     CHANCES["Common"] = max(7, BASE_CHANCES["Common"] - BONUS_TOTAL)
     BONUS_RECALCULADO = BASE_CHANCES["Common"] - CHANCES["Common"]
@@ -56,20 +97,9 @@ def calcular_raridades():
             CHANCES["Rare"] += BONUS_RECALCULADO * 0.46
     return CHANCES
 
-def limpar_tela():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def startup():
-    limpar_tela()
-    print("====================")
-    print("\n  PY FISHING GAME")
-    print("  v. A01\n")
-    print("====================")
-    os.system("pause")
-    limpar_tela()
-
 def gerar_peixe():
     CHANCES = calcular_raridades()
+    prarity = ""
     x = random.uniform(0,100)
     if x <= CHANCES["Secret"]:
         prarity = "Secret"
@@ -83,43 +113,52 @@ def gerar_peixe():
         prarity = "Uncommon"
     else:
         prarity = "Common"
-    return prarity
+    psize = random.uniform(FISHES_SIZES[prarity][0],FISHES_SIZES[prarity][1])
+    return prarity, psize
 
 def pesca():
     limpar_tela()
-    prarity = gerar_peixe()
-    tempo = 5
+    prarity, psize = gerar_peixe()
+    tempo = int((random.uniform(1,5) / (PLAYER_STATS["str"] + PLAYER_STATS["speed"])))
     print("PESCA\n")
     print("PESCANDO: ", end="")
     for i in range(tempo):
-        print(".", end="", flush=True)
+        print("■", end="", flush=True)
         time.sleep(tempo / 10)
         if i == tempo - 1:
             print("\n")
     peixe = random.randint(0,(len(PEIXES[prarity]) - 1))
-    print(f"[{prarity}]\n{PEIXES[prarity][peixe]}\n")
+    peixeNome = PEIXES[prarity][peixe]
+    if peixeNome not in PLAYER_FISHES:
+        print("NOVA DESCOBERTA!")
+        PLAYER_FISHES[peixeNome] = psize
+    else:
+        if psize > PLAYER_FISHES[peixeNome]:
+            PLAYER_FISHES[peixeNome] = psize
+            print("NOVO RECORDE!")
+    print(f"[{prarity}]\n{peixeNome}\n{psize:.2f} cm")
     os.system("pause")
 
 startup()
 while(True):
     option = -1
-    while(option < 0):
-        limpar_tela()
-        print("MENU")
-        print("[1] PESCA")
-        print("[2] INVENTARIO")
-        print("[3] LOJA")
-        print("[4] HABILIDADES")
-        print("[0] FINALIZAR JOGO")
+    limpar_tela()
+    print("PY FISH GAME v.A01\n")
+    player_status_menu()
+    print("\n[1] PESCA       [2] INVENTARIO")
+    print("[3] LOJA        [4] HABILIDADES")
+    print("[0] SAIR")
+    try:
         option = int(input("\nESCOLHA: "))
-    if (option == 0):
-        limpar_tela()
-        print("OBRIGADO POR JOGAR!\n")
-        break
-    elif (option == 1):
-        pesca()
-    elif (option == 999):
-        print("\nDEBUG RARIDADES")
-        CHANCES = calcular_raridades()
-        print("Secret: {}\nLegendary: {}\nEpic: {}\nRare: {}\nUncommon: {}\nCommon: {}\n".format(CHANCES["Secret"], CHANCES["Legendary"], CHANCES["Epic"], CHANCES["Rare"], CHANCES["Uncommon"], CHANCES["Common"]))
-        os.system("pause")
+        if (option == 0):
+            print("OBRIGADO POR JOGAR!\n")
+            break
+        elif (option == 1):
+            pesca()
+        elif (option == 999):
+            print("\nDEBUG RARIDADES")
+            CHANCES = calcular_raridades()
+            print("Secret: {}\nLegendary: {}\nEpic: {}\nRare: {}\nUncommon: {}\nCommon: {}\n".format(CHANCES["Secret"], CHANCES["Legendary"], CHANCES["Epic"], CHANCES["Rare"], CHANCES["Uncommon"], CHANCES["Common"]))
+            os.system("pause")
+    except ValueError:
+        option = -1
